31b9d77927a30e1f0dacea0f7bd2a4cf8fe77346
dylan <dylanelens@hotmail.com>
2022-07-05 16:20:43 +0200

    small fix

--- lua/options.lua
+++ lua/options.lua
@@ -21,6 +21,9 @@ local options = {
   undofile = true,                         -- enable persistent undo
   updatetime = 300,                        -- faster completion (4000ms default)
   writebackup = false,                     -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
+  expandtab = true,                        -- convert tabs to spaces
+  shiftwidth = 2,                          -- the number of spaces inserted for each indentation
+  tabstop = 2,                             -- insert 2 spaces for a tab
   cursorline = false,                       -- highlight the current line
   number = true,                           -- set numbered lines
   relativenumber = true,                  -- set relative numbered lines
@@ -28,8 +31,8 @@ local options = {
   signcolumn = "yes",                      -- always show the sign column, otherwise it would shift the text each time
   wrap = false,                            -- display lines as one long line
   scrolloff = 8,                           -- is one of my fav
+  guicursor = "i:block",
   sidescrolloff = 8,
-  guicursor = "i:block"
 }
 
 vim.opt.shortmess:append "c"
@@ -37,4 +40,5 @@ vim.opt.shortmess:append "c"
 for k, v in pairs(options) do
   vim.opt[k] = v
 end
+
 
