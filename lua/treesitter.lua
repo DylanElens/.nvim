7de73efae525b9ee7bac838eb5bde8d3d02f7bde
Samuel Padron <spadron05@gmail.com>
2024-06-29 23:41:25 +0200

    Changed to lazy vim and kickstart vim config

--- lua/treesitter.lua
+++
@@ -1,9 +0,0 @@
-require'nvim-treesitter.configs'.setup {
-
-  ensure_installed = {"javascript", "typescript", "c", "rust"},
-
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = false,
-  },
-}
